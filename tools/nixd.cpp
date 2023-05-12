6db965a1e57d0fbe3d6502d6ac31c6136f6f08d6
Yingchi Long <i@lyc.dev>
2023-05-12 22:51:20 +0800

    tools/nixd: remove hardcoded path at nixd startup
    We can communicate with the client extension via stderr after initialize
    implemented.

--- tools/nixd.cpp
+++ tools/nixd.cpp
@@ -3,16 +3,7 @@
 #include "lspserver/Logger.h"
 #include "nixd/Server.h"
 
-#include <llvm/Support/FileSystem.h>
-#include <llvm/Support/raw_ostream.h>
-#include <system_error>
-
 int main() {
-  std::error_code EC;
-  auto DebugOut = llvm::raw_fd_ostream("/tmp/nixd.out", EC,
-                                       llvm::sys::fs::OpenFlags::OF_Append);
-  lspserver::StreamLogger Logger(DebugOut, lspserver::Logger::Debug);
-  lspserver::LoggingSession LoggingSession(Logger);
   lspserver::log("Server started.");
   nixd::Server Server{std::make_unique<lspserver::InboundPort>(),
                       std::make_unique<lspserver::OutboundPort>()};
