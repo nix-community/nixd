nix-community/nixd

commit e15dd077b2660a29dd75ef64f58a10b60bf1dab9

lsp: binder for method calls & notification (#6)

lib/lspserver/include/lspserver/LSPServer.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "lspserver/Connection.h"
+#include "lspserver/Function.h"
 #include "lspserver/LSPBinder.h"
+
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/Error.h>
 #include <llvm/Support/JSON.h>
+
 #include <memory>
 
 namespace lspserver {
 
+/// LSPServer wraps inputs & outputs, associate message IDs between calls/reply,
+/// and provide type-safe interfaces.
 class LSPServer : public MessageHandler {
 private:
   std::unique_ptr<InboundPort> In;
@@ -16,12 +23,60 @@ private:
   bool onCall(llvm::StringRef Method, llvm::json::Value Params,
               llvm::json::Value ID) override;
   bool onReply(llvm::json::Value ID,
-               llvm::Expected<llvm::json::Value> Result) override {
-    return true;
+               llvm::Expected<llvm::json::Value> Result) override;
+
+  std::mutex PendingCallsLock;
+
+  /// Calls to the client sent, and waiting for the response.
+  /// The callback function will be invoked when we get the result.
+  ///
+  /// If the call has no response for a long time, it should be removed and
+  /// associated an error.
+  std::map<int, Callback<llvm::json::Value>> PendingCalls;
+
+  /// Number of maximum callbacks stored in the structure.
+  /// Give an error to the oldest callback (least ID) while exceeding this
+  /// value.
+  static constexpr int MaxPendingCalls = 100;
+
+  int TopID;
+
+  /// Allocate an "ID" (as returned value) for this callback.
+  int bindReply(Callback<llvm::json::Value>);
+
+  void callMethod(llvm::StringRef Method, llvm::json::Value Params,
+                  Callback<llvm::json::Value> CB) {
+    llvm::json::Value ID{bindReply(std::move(CB))};
+    Out->call(Method, Params, ID);
   }
 
 protected:
   HandlerRegistry Registry;
+
+  /// \return a unique_function that could be invoked to notify the client (i.e.
+  /// no replies are expected).
+  template <class T>
+  llvm::unique_function<void(const T &)>
+  mkOutNotifiction(llvm::StringRef Method) {
+    return [=](const T &Params) { Out->notify(Method, Params); };
+  }
+
+  /// \return a unique_function that could be invoked to "call" the client (i.e.
+  /// clients are expected to reply our requests).
+  template <class ParamTy, class ResponseTy>
+  llvm::unique_function<void(const ParamTy &, Callback<ResponseTy>)>
+  mkOutMethod(llvm::StringRef Method) {
+    return [&](const ParamTy &Params, Callback<ResponseTy> Reply) {
+      callMethod(Method, Params,
+                 [=, Reply = std::move(Reply)](
+                     llvm::Expected<llvm::json::Value> Response) {
+                   if (!Response)
+                     return Reply(Response.takeError());
+                   Reply(parseParam<ResponseTy>(std::move(*Response), Method,
+                                                "reply"));
+                 });
+    };
+  }
 
 public:
   LSPServer(std::unique_ptr<InboundPort> In, std::unique_ptr<OutboundPort> Out)

lib/lspserver/include/lspserver/LSPBinder.h
@@ -9,6 +9,28 @@
 
 namespace lspserver {
 
+template <typename T>
+llvm::Expected<T> parseParam(const llvm::json::Value &Raw,
+                             llvm::StringRef PayloadName,
+                             llvm::StringRef PayloadKind) {
+  T Result;
+  llvm::json::Path::Root Root;
+  if (!fromJSON(Raw, Result, Root)) {
+    elog("Failed to decode {0} {1}: {2}", PayloadName, PayloadKind,
+         Root.getError());
+    // Dump the relevant parts of the broken message.
+    std::string Context;
+    llvm::raw_string_ostream OS(Context);
+    Root.printErrorContext(Raw, OS);
+    vlog("{0}", OS.str());
+    // Report the error (e.g. to the client).
+    return llvm::make_error<LSPError>(
+        llvm::formatv("failed to decode {0} {1}: {2}", PayloadName, PayloadKind,
+                      fmt_consume(Root.getError())),
+        ErrorCode::InvalidParams);
+  }
+  return std::move(Result);
+}
 struct HandlerRegistry {
   using JSON = llvm::json::Value;
   template <typename HandlerT>
@@ -19,29 +41,6 @@ struct HandlerRegistry {
   HandlerMap<void(JSON, Callback<JSON>)> CommandHandlers;
 
 public:
-  template <typename T>
-  static llvm::Expected<T> parse(const llvm::json::Value &Raw,
-                                 llvm::StringRef PayloadName,
-                                 llvm::StringRef PayloadKind) {
-    T Result;
-    llvm::json::Path::Root Root;
-    if (!fromJSON(Raw, Result, Root)) {
-      elog("Failed to decode {0} {1}: {2}", PayloadName, PayloadKind,
-           Root.getError());
-      // Dump the relevant parts of the broken message.
-      std::string Context;
-      llvm::raw_string_ostream OS(Context);
-      Root.printErrorContext(Raw, OS);
-      vlog("{0}", OS.str());
-      // Report the error (e.g. to the client).
-      return llvm::make_error<LSPError>(
-          llvm::formatv("failed to decode {0} {1}: {2}", PayloadName,
-                        PayloadKind, fmt_consume(Root.getError())),
-          ErrorCode::InvalidParams);
-    }
-    return std::move(Result);
-  }
-
   /// Bind a handler for an LSP method.
   /// e.g. method("peek", this, &ThisModule::peek);
   /// Handler should be e.g. void peek(const PeekParams&, Callback<PeekResult>);
@@ -51,7 +50,7 @@ public:
                  void (ThisT::*Handler)(const Param &, Callback<Result>)) {
     MethodHandlers[Method] = [Method, Handler, This](JSON RawParams,
                                                      Callback<JSON> Reply) {
-      auto P = parse<Param>(RawParams, Method, "request");
+      auto P = parseParam<Param>(RawParams, Method, "request");
       if (!P)
         return Reply(P.takeError());
       (This->*Handler)(*P, std::move(Reply));
@@ -66,7 +65,7 @@ public:
   void addNotification(llvm::StringLiteral Method, ThisT *This,
                        void (ThisT::*Handler)(const Param &)) {
     NotificationHandlers[Method] = [Method, Handler, This](JSON RawParams) {
-      llvm::Expected<Param> P = parse<Param>(RawParams, Method, "request");
+      llvm::Expected<Param> P = parseParam<Param>(RawParams, Method, "request");
       if (!P)
         return llvm::consumeError(P.takeError());
       (This->*Handler)(*P);
@@ -82,98 +81,12 @@ public:
                   void (ThisT::*Handler)(const Param &, Callback<Result>)) {
     CommandHandlers[Command] = [Command, Handler, This](JSON RawParams,
                                                         Callback<JSON> Reply) {
-      auto P = parse<Param>(RawParams, Command, "command");
+      auto P = parseParam<Param>(RawParams, Command, "command");
       if (!P)
         return Reply(P.takeError());
       (This->*Handler)(*P, std::move(Reply));
     };
   }
 };
-
-/// LSPBinder collects a table of functions that handle LSP calls.
-///
-/// It translates a handler method's signature, e.g.
-///    void codeComplete(CompletionParams, Callback<CompletionList>)
-/// into a wrapper with a generic signature:
-///    void(json::Value, Callback<json::Value>)
-/// The wrapper takes care of parsing/serializing responses.
-///
-/// Incoming calls can be methods, notifications, or commands - all are similar.
-///
-/// FIXME: this should also take responsibility for wrapping *outgoing* calls,
-/// replacing the typed ClangdLSPServer::call<> etc.
-class LSPBinder {
-public:
-  using JSON = llvm::json::Value;
-  class RawOutgoing {
-  public:
-    virtual ~RawOutgoing() = default;
-    virtual void callMethod(llvm::StringRef Method, JSON Params,
-                            Callback<JSON> Reply) = 0;
-    virtual void notify(llvm::StringRef Method, JSON Params) = 0;
-  };
-
-  LSPBinder(RawOutgoing &Out) : Out(Out) {}
-
-  template <typename P, typename R>
-  using OutgoingMethod = llvm::unique_function<void(const P &, Callback<R>)>;
-  /// UntypedOutgoingMethod is convertible to OutgoingMethod<P, R>.
-  class UntypedOutgoingMethod;
-  /// Bind a function object to be used for outgoing method calls.
-  /// e.g. OutgoingMethod<EParams, EResult> Edit = Bind.outgoingMethod("edit");
-  /// EParams must be JSON-serializable, EResult must be parseable.
-  UntypedOutgoingMethod outgoingMethod(llvm::StringLiteral Method);
-
-  template <typename P>
-  using OutgoingNotification = llvm::unique_function<void(const P &)>;
-  /// UntypedOutgoingNotification is convertible to OutgoingNotification<T>.
-  class UntypedOutgoingNotification;
-  /// Bind a function object to be used for outgoing notifications.
-  /// e.g. OutgoingNotification<LogParams> Log = Bind.outgoingMethod("log");
-  /// LogParams must be JSON-serializable.
-  UntypedOutgoingNotification outgoingNotification(llvm::StringLiteral Method);
-
-private:
-  RawOutgoing &Out;
-};
-
-class LSPBinder::UntypedOutgoingNotification {
-  llvm::StringLiteral Method;
-  RawOutgoing *Out;
-  UntypedOutgoingNotification(llvm::StringLiteral Method, RawOutgoing *Out)
-      : Method(Method), Out(Out) {}
-  friend UntypedOutgoingNotification
-      LSPBinder::outgoingNotification(llvm::StringLiteral);
-
-public:
-  template <typename Request> operator OutgoingNotification<Request>() && {
-    return
-        [Method(Method), Out(Out)](Request R) { Out->notify(Method, JSON(R)); };
-  }
-};
-
-inline LSPBinder::UntypedOutgoingNotification
-LSPBinder::outgoingNotification(llvm::StringLiteral Method) {
-  return UntypedOutgoingNotification(Method, &Out);
-}
-
-class LSPBinder::UntypedOutgoingMethod {
-  llvm::StringLiteral Method;
-  RawOutgoing *Out;
-  UntypedOutgoingMethod(llvm::StringLiteral Method, RawOutgoing *Out)
-      : Method(Method), Out(Out) {}
-  friend UntypedOutgoingMethod LSPBinder::outgoingMethod(llvm::StringLiteral);
-
-public:
-  template <typename Request, typename Response>
-  operator OutgoingMethod<Request, Response>() && {
-    return [Method(Method), Out(Out)](Request R, Callback<Response> Reply) {};
-  }
-};
-
-inline LSPBinder::UntypedOutgoingMethod
-LSPBinder::outgoingMethod(llvm::StringLiteral Method) {
-  return UntypedOutgoingMethod(Method, &Out);
-}
 
 } // namespace lspserver

lib/lspserver/src/LSPServer.cpp
@@ -1,6 +1,12 @@
 #include "lspserver/LSPServer.h"
 #include "lspserver/Connection.h"
+#include "lspserver/Function.h"
+
+#include <llvm/ADT/FunctionExtras.h>
 #include <llvm/Support/Error.h>
+#include <llvm/Support/JSON.h>
+
+#include <mutex>
 
 namespace lspserver {
 
@@ -34,6 +40,42 @@ bool LSPServer::onCall(llvm::StringRef Method, llvm::json::Value Params,
   else
     return false;
   return true;
+}
+
+bool LSPServer::onReply(llvm::json::Value ID,
+                        llvm::Expected<llvm::json::Value> Result) {
+  log("<-- reply({0})", ID);
+  if (auto I = ID.getAsInteger()) {
+    if (PendingCalls.contains(*I)) {
+      PendingCalls[*I](std::move(Result));
+      PendingCalls.erase(*I);
+      return true;
+    }
+    elog("received a reply with ID {0}, but there was no such call", ID);
+    // Ignore this error
+    return true;
+  }
+  elog("Cannot retrieve message ID from json: {0}", ID);
+  return false;
+}
+
+int LSPServer::bindReply(Callback<llvm::json::Value> CB) {
+  std::lock_guard<std::mutex> _(PendingCallsLock);
+  int Ret = TopID++;
+  PendingCalls[Ret] = std::move(CB);
+
+  // Check the limit
+  if (PendingCalls.size() > MaxPendingCalls) {
+    auto Begin = PendingCalls.begin();
+    auto [ID, OldestCallback] =
+        std::tuple{Begin->first, std::move(Begin->second)};
+    OldestCallback(
+        error("failed to receive a client reply for request ({0})", ID));
+    elog("more than {0} outstanding LSP calls, forgetting about {1}",
+         MaxPendingCalls, ID);
+    PendingCalls.erase(Begin);
+  }
+  return Ret;
 }
 
 } // namespace lspserver

commit b778d6f6ab5736a87e20fb828fe61922e2b9f6e1

lspserver: extra fixup for outgoing bindings
These changes are addressing logging & implementation defect introduced
in

    e15dd07 (lsp: binder for method calls & notification (#6), 2023-05-13)

Link: #6

lib/lspserver/include/lspserver/LSPServer.h
@@ -42,14 +42,15 @@ private:
   /// value.
   static constexpr int MaxPendingCalls = 100;
 
-  int TopID;
+  int TopID = 1;
 
   /// Allocate an "ID" (as returned value) for this callback.
   int bindReply(Callback<llvm::json::Value>);
 
   void callMethod(llvm::StringRef Method, llvm::json::Value Params,
                   Callback<llvm::json::Value> CB) {
-    llvm::json::Value ID{bindReply(std::move(CB))};
+    llvm::json::Value ID(bindReply(std::move(CB)));
+    log("--> call {0}({1})", Method, ID.getAsInteger());
     Out->call(Method, Params, ID);
   }
 
@@ -59,6 +60,7 @@ protected:
   llvm::unique_function<void(const T &)>
   mkOutNotifiction(llvm::StringRef Method) {
     return [Method = Method, this](const T &Params) {
+      log("--> notify {0}", Method);
       Out->notify(Method, Params);
     };
   }
@@ -66,10 +68,10 @@ protected:
   template <class ParamTy, class ResponseTy>
   llvm::unique_function<void(const ParamTy &, Callback<ResponseTy>)>
   mkOutMethod(llvm::StringRef Method) {
-    return [&](const ParamTy &Params, Callback<ResponseTy> Reply) {
+    return [=](const ParamTy &Params, Callback<ResponseTy> Reply) {
       callMethod(Method, Params,
                  [=, Reply = std::move(Reply)](
-                     llvm::Expected<llvm::json::Value> Response) {
+                     llvm::Expected<llvm::json::Value> Response) mutable {
                    if (!Response)
                      return Reply(Response.takeError());
                    Reply(parseParam<ResponseTy>(std::move(*Response), Method,