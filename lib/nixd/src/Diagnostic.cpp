6be40c733f892dee4e45d954d5ae9a662ac6dc93
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-05-15 22:54:24 +0800

    nixd: remove ANSI color from nix output, but not add unit test

--- lib/nixd/src/Diagnostic.cpp
+++ lib/nixd/src/Diagnostic.cpp
@@ -2,6 +2,30 @@
 
 #include "lspserver/Protocol.h"
 
+using std::string;
+#define __REMOVED_ANSI_STR__(_) \
+_("\e[0m") \
+_("\e[1m") \
+_("\e[2m") \
+_("\e[3m") \
+_("\e[31;1m") \
+_("\e[32;1m") \
+_("\e[35;1m") \
+_("\e[34;1m") \
+_("\e[35;1m") \
+_("\e[36;1m") \
+
+static std::string ansiFilter(std::string Msg){
+    #define __REMOVE_ANSI_STR_CODE__(ansi_str) \
+    do { \
+        auto Pos = Msg.find(ansi_str); \
+        if (Pos==string::npos) break; \
+        Msg.erase(Pos, std::strlen(ansi_str)); \
+    } while(1);
+    __REMOVED_ANSI_STR__(__REMOVE_ANSI_STR_CODE__)
+    return Msg;
+}
+
 namespace nixd {
 
 std::vector<lspserver::Diagnostic> mkDiagnostics(const nix::Error &PE) {
@@ -11,7 +35,7 @@ std::vector<lspserver::Diagnostic> mkDiagnostics(const nix::Error &PE) {
       ErrPos ? translatePosition(*ErrPos) : lspserver::Position{};
   Ret.push_back(lspserver::Diagnostic{.range = {.start = ErrLoc, .end = ErrLoc},
                                       .severity = /* Error */ 1,
-                                      .message = PE.info().msg.str()});
+                                      .message = ansiFilter(PE.info().msg.str())});
   return Ret;
 }
 
