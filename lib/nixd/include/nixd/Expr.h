36e5515c752602703303855ecfeffe5a011a09b3
Yingchi Long <i@lyc.dev>
2023-05-15 18:51:27 +0800

    nixd: add AST visitor library

---
+++ lib/nixd/include/nixd/Expr.h
@@ -0,0 +1,59 @@
+/// 'nix::Expr' wrapper that suitable for language server
+#pragma once
+
+#include <nix/nixexpr.hh>
+
+namespace nixd {
+
+template <class Derived> struct RecursiveASTVisitor {
+
+  bool shouldTraversePostOrder() { return false; }
+
+#define NIX_EXPR(EXPR) bool traverse##EXPR(const nix::EXPR *E);
+#include "NixASTNodes.inc"
+#undef NIX_EXPR
+
+#define NIX_EXPR(EXPR)                                                         \
+  bool visit##EXPR(const nix::EXPR *E) { return true; }
+#include "NixASTNodes.inc"
+#undef NIX_EXPR
+
+  Derived &getDerived() { return *static_cast<Derived *>(this); }
+
+  bool traverseExpr(const nix::Expr *E) {
+    if (!E)
+      return true;
+#define NIX_EXPR(EXPR)                                                         \
+  if (auto CE = dynamic_cast<const nix::EXPR *>(E)) {                          \
+    return getDerived().traverse##EXPR(CE);                                    \
+  }
+#include "NixASTNodes.inc"
+    assert(false && "We are missing some nix AST Nodes!");
+    return true;
+#undef NIX_EXPR
+  }
+}; // namespace nixd
+
+#define TRY_TO(CALL_EXPR)                                                      \
+  do {                                                                         \
+    if (!getDerived().CALL_EXPR)                                               \
+      return false;                                                            \
+  } while (false)
+
+#define TRY_TO_TRAVERSE(EXPR) TRY_TO(traverseExpr(EXPR))
+
+#define DEF_TRAVERSE_TYPE(TYPE, CODE)                                          \
+  template <typename Derived>                                                  \
+  bool RecursiveASTVisitor<Derived>::traverse##TYPE(const nix::TYPE *T) {      \
+    if (!getDerived().shouldTraversePostOrder())                               \
+      TRY_TO(visit##TYPE(T));                                                  \
+    { CODE; }                                                                  \
+    if (getDerived().shouldTraversePostOrder())                                \
+      TRY_TO(visit##TYPE(T));                                                  \
+    return true;                                                               \
+  }
+#include "NixASTTraverse.inc"
+#undef DEF_TRAVERSE_TYPE
+#undef TRY_TO_TRAVERSE
+#undef TRY_TO
+} // namespace nixd
---
+++ lib/nixd/test/expr.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+
+#include "nixd/Expr.h"
+#include "nixutil.h"
+
+#include <nix/eval.hh>
+#include <nix/shared.hh>
+
+namespace nixd {
+
+TEST(Expr, Visitor1) {
+  static const char *NixSrc = R"(
+let
+    pkgs = { a = 1; };
+in
+with pkgs;
+a
+  )";
+  InitNix N;
+  struct MyVisitor : nixd::RecursiveASTVisitor<MyVisitor> {
+    bool VisitedWith = false;
+    bool VisitedLet = false;
+
+    bool visitExprLet(const nix::ExprLet *EL) {
+      VisitedLet = true;
+      return true;
+    }
+    bool visitExprWith(const nix::ExprWith *EW) {
+      VisitedWith = true;
+      return true;
+    }
+  } Visitor;
+
+  auto State = N.getDummyState();
+  auto *E = State->parseExprFromString(NixSrc, "/");
+  ASSERT_TRUE(Visitor.traverseExpr(E));
+  ASSERT_TRUE(Visitor.VisitedWith);
+  ASSERT_TRUE(Visitor.VisitedLet);
+}
+
+TEST(Expr, Visitor2) {
+  static const char *NixSrc = R"(
+let
+  x = 1;
+in
+rec {
+  integerAnd = 1 && 0;
+  floatAdd = 1.5 + 2.0;
+  ifElse = if 1 then 1 else 0;
+  update = { } // { };
+  opEq = 1 == 2;
+  opNEq = 1 != 2;
+  opImpl = 1 -> 2;
+  opNot = ! false;
+  opOr = 1 || 2;
+  lambda = { }: { };
+  someAttrSet = { a = 1; };
+  hasAttr = someAttrSet ? "a";
+  string = "someString";
+  listConcat = [ 1 2 ] ++ [ 3 4 ];
+  concat = "a" + "b";
+  path = ./.;
+  exprDiv = 5 / 3;
+  exprPos = __curPos;
+  exprAssert = assert true ; "some message";
+  select = someAttrSet.a;
+  exprWith = with someAttrSet; [ ];
+}
+  )";
+  InitNix N;
+  struct MyVisitor : nixd::RecursiveASTVisitor<MyVisitor> {
+#define NIX_EXPR(EXPR) bool Visited##EXPR = false;
+#include "nixd/NixASTNodes.inc"
+#undef NIX_EXPR
+
+#define NIX_EXPR(EXPR)                                                         \
+  bool visit##EXPR(const nix::EXPR *E) {                                       \
+    Visited##EXPR = true;                                                      \
+    return true;                                                               \
+  }
+#include "nixd/NixASTNodes.inc"
+#undef NIX_EXPR
+
+  } Visitor;
+
+  auto State = N.getDummyState();
+  auto *E = State->parseExprFromString(NixSrc, "/");
+  ASSERT_TRUE(Visitor.traverseExpr(E));
+#define NIX_EXPR(EXPR) ASSERT_TRUE(Visitor.Visited##EXPR);
+#include "nixd/NixASTNodes.inc"
+#undef NIX_EXPR
+}
+
+} // namespace nixd
