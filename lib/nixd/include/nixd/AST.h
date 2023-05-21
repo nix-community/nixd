21d5d8dd9a971f61bf49f8eaa93449ca69c6d24c
Yingchi Long <i@lyc.dev>
2023-05-21 23:40:21 +0800

    nixd/AST: remove evaluation async method (withEvaluation)
    Nix evaluation is on an *workspace*. Thus, it is not suitable for us to
    having this method in AST.
    
    Also renamed to `EvalAST`, what does 'Nix' mean in `NixAST`?

--- lib/nixd/include/nixd/AST.h
+++ lib/nixd/include/nixd/AST.h
@@ -2,26 +2,31 @@
 
 #include "CallbackExpr.h"
 
+#include "eval.hh"
 #include "lspserver/Path.h"
 #include "lspserver/Protocol.h"
+#include "ref.hh"
 
 #include <llvm/ADT/FunctionExtras.h>
 #include <llvm/ADT/StringRef.h>
 
+#include <map>
+#include <memory>
 #include <nix/error.hh>
 #include <nix/installable-value.hh>
 #include <nix/installables.hh>
 #include <nix/nixexpr.hh>
 
 #include <boost/asio.hpp>
+#include <queue>
+#include <utility>
 
 namespace nixd {
 
 /// A Nix language AST wrapper that support language features for LSP.
-class NixAST {
-#ifndef NDEBUG
-  bool Evaluated = false;
-#endif
+/// Most features are available after evaluation.
+class EvalAST {
+  ASTContext Cxt;
   nix::Expr *Root;
   std::map<const nix::Expr *, nix::Value> ValueMap;
   std::map<const nix::Expr *, nix::Env> EnvMap;
@@ -31,51 +36,21 @@ public:
   void injectAST(nix::EvalState &State, lspserver::PathRef Path);
 
   /// From fresh-parsed nix::Expr *AST root, e.g. State->parseExprFromString
-  NixAST(ASTContext &Cxt, nix::Expr *Root);
+  EvalAST(nix::Expr *Root);
 
   /// Get the evaluation result (fixed point) of the expression.
-  nix::Value getValue(nix::Expr *Expr) {
-    assert(Evaluated && "must be called after evaluation!");
-    return ValueMap[Expr];
-  }
+  nix::Value getValue(nix::Expr *Expr) { return ValueMap[Expr]; }
 
   /// Get the corresponding 'Env' while evaluating the expression.
   /// nix 'Env's contains dynamic variable name bindings at evaluation, might be
   /// used for completion.
-  nix::Env getEnv(nix::Expr *Expr) {
-    assert(Evaluated && "must be called after evaluation!");
-    return EnvMap[Expr];
-  }
+  nix::Env getEnv(nix::Expr *Expr) { return EnvMap[Expr]; }
 
   /// Lookup an AST node located at the position.
   /// Runs in O(n) (inefficient!)
   nix::Expr *lookupPosition(const nix::EvalState &State, lspserver::Position);
 
   nix::Expr *root() { return Root; }
-
-  /// Ensure the evaluation task has been done, and invokes callback with `this`
-  /// pointer. Some features, like `Env`s, `Value`s, are only available after
-  /// evaluation. Evaluation errors shall be passed into the first argument.
-  void withEvaluation(
-      boost::asio::thread_pool &Pool, nix::ref<nix::InstallableValue> IValue,
-      nix::ref<nix::EvalState> State,
-      llvm::unique_function<void(std::exception *, NixAST *)> Result);
-};
-
-/// Parsing ASTs on each document version, providing ASTs & evaluation analysis
-/// on requests
-class ASTManager {
-
-public:
-  /// Parse the AST, any parsing errors will raise an exception.
-  /// \p BasicEval indicates that whether to provide an standalone evaluation.
-  /// (Nix files are syntax units, and should be evaluated correctly though)
-  void parseAST(lspserver::PathRef File, std::string Contents,
-                llvm::StringRef Version, bool BasicEval = true);
-
-  /// Get the AST of the file. If there are some errors occurred before, returns
-  /// nullptr.
-  NixAST *getAST(lspserver::PathRef File, llvm::StringRef Version);
 };
 
 } // namespace nixd
--- lib/nixd/src/AST.cpp
+++ lib/nixd/src/AST.cpp
@@ -2,13 +2,14 @@
 #include "nixd/CallbackExpr.h"
 
 #include "lspserver/Logger.h"
+#include "nixd/Expr.h"
 
 #include <exception>
 #include <memory>
 
 namespace nixd {
 
-NixAST::NixAST(ASTContext &Cxt, nix::Expr *Root) {
+EvalAST::EvalAST(nix::Expr *Root) : Cxt(ASTContext()) {
   auto EvalCallback = [this](const nix::Expr *Expr, const nix::EvalState &,
                              const nix::Env &ExprEnv,
                              const nix::Value &ExprValue) {
@@ -18,37 +19,9 @@ NixAST::NixAST(ASTContext &Cxt, nix::Expr *Root) {
   this->Root = rewriteCallback(Cxt, EvalCallback, Root);
 }
 
-void NixAST::injectAST(nix::EvalState &State, lspserver::PathRef Path) {
+void EvalAST::injectAST(nix::EvalState &State, lspserver::PathRef Path) {
   nix::Value DummyValue{};
   State.cacheFile(Path.str(), Path.str(), Root, DummyValue);
-}
-
-void NixAST::withEvaluation(
-    boost::asio::thread_pool &Pool, nix::ref<nix::InstallableValue> IValue,
-    nix::ref<nix::EvalState> State,
-    llvm::unique_function<void(std::exception *, NixAST *)> Result) {
-  auto Action = [this,
-                 Result = std::move(Result)](std::exception *Except) mutable {
-    if (Except == nullptr) {
-#ifndef NDEBUG
-      Evaluated = true;
-#endif
-      Result(nullptr, this);
-    } else {
-      Result(Except, nullptr);
-    }
-  };
-  boost::asio::post(Pool,
-                    [IValue, Action = std::move(Action), State]() mutable {
-                      try {
-                        IValue->toValue(*State);
-                        Action(nullptr);
-                      } catch (std::exception &Except) {
-                        lspserver::log(Except.what());
-                        Action(&Except);
-                      }
-                    });
-  Pool.join();
 }
 
 } // namespace nixd
