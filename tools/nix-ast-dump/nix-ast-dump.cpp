2cff60d8c23b29a9860f7dedf2b60700886f8818
Yingchi Long <i@lyc.dev>
2023-05-27 20:39:20 +0800

    tools: add `nix-ast-dump` (#16)
    also add an ast dump utility, produces indented output:
    
    ```console
    [user@system]$ build/nix-ast-dump ~flakes/shell.nix
     ExprLambda: ({ pkgs ? (let lock = (((builtins).fromJSON ((builtins).readFile ~flakes/flake.lock))).nodes.nixpkgs.locked; nixpkgs = (fetchTarball { sha256 = (lock).narHash; url = ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz"); }); in (import nixpkgs { overlays = [ ]; })), ... }: ((pkgs).mkShell { NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake"; nativeBuildInputs = (with pkgs; [ (nix) (home-manager) (git) (sops) (gnupg) (age) ]); }))
      ExprLet: (let lock = (((builtins).fromJSON ((builtins).readFile ~flakes/flake.lock))).nodes.nixpkgs.locked; nixpkgs = (fetchTarball { sha256 = (lock).narHash; url = ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz"); }); in (import nixpkgs { overlays = [ ]; }))
       ExprAttrs: { lock = (((builtins).fromJSON ((builtins).readFile ~flakes/flake.lock))).nodes.nixpkgs.locked; nixpkgs = (fetchTarball { sha256 = (lock).narHash; url = ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz"); }); }
        ExprSelect: (((builtins).fromJSON ((builtins).readFile ~flakes/flake.lock))).nodes.nixpkgs.locked
         ExprCall: ((builtins).fromJSON ((builtins).readFile ~flakes/flake.lock))
          ExprCall: ((builtins).readFile ~flakes/flake.lock)
           ExprPath: ~flakes/flake.lock
           ExprSelect: (builtins).readFile
            ExprVar: builtins
          ExprSelect: (builtins).fromJSON
           ExprVar: builtins
        ExprCall: (fetchTarball { sha256 = (lock).narHash; url = ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz"); })
         ExprAttrs: { sha256 = (lock).narHash; url = ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz"); }
          ExprConcatStrings: ("https://github.com/nixos/nixpkgs/archive/" + (lock).rev + ".tar.gz")
           ExprString: "https://github.com/nixos/nixpkgs/archive/"
           ExprSelect: (lock).rev
            ExprVar: lock
           ExprString: ".tar.gz"
          ExprSelect: (lock).narHash
           ExprVar: lock
         ExprVar: fetchTarball
       ExprCall: (import nixpkgs { overlays = [ ]; })
        ExprVar: nixpkgs
        ExprAttrs: { overlays = [ ]; }
         ExprList: [ ]
        ExprVar: import
      ExprCall: ((pkgs).mkShell { NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake"; nativeBuildInputs = (with pkgs; [ (nix) (home-manager) (git) (sops) (gnupg) (age) ]); })
       ExprAttrs: { NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake"; nativeBuildInputs = (with pkgs; [ (nix) (home-manager) (git) (sops) (gnupg) (age) ]); }
        ExprString: "extra-experimental-features = nix-command flakes repl-flake"
        ExprWith: (with pkgs; [ (nix) (home-manager) (git) (sops) (gnupg) (age) ])
         ExprVar: pkgs
         ExprList: [ (nix) (home-manager) (git) (sops) (gnupg) (age) ]
          ExprVar: nix
          ExprVar: home-manager
          ExprVar: git
          ExprVar: sops
          ExprVar: gnupg
          ExprVar: age
       ExprSelect: (pkgs).mkShell
        ExprVar: pkgs
    ```
    
    ```nix
    # Shell for bootstrapping flake-enabled nix and other tooling
    { pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
      let
        lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
        nixpkgs = fetchTarball {
          url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
          sha256 = lock.narHash;
        };
      in
      import nixpkgs { overlays = [ ]; }
    , ...
    }: pkgs.mkShell {
      NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
      nativeBuildInputs = with pkgs; [
        nix
        home-manager
        git
    
        sops
        gnupg
        age
      ];
    }
    ```

---
+++ tools/nix-ast-dump/nix-ast-dump.cpp
@@ -0,0 +1,58 @@
+#include "nixd/Expr.h"
+
+#include <nix/eval.hh>
+#include <nix/nixexpr.hh>
+#include <nix/shared.hh>
+#include <nix/store-api.hh>
+
+#include <iostream>
+#include <memory>
+
+class InitNix {
+public:
+  InitNix() {
+    nix::initNix();
+    nix::initGC();
+  }
+  std::unique_ptr<nix::EvalState> getDummyState() {
+    return std::make_unique<nix::EvalState>(nix::Strings{},
+                                            nix::openStore("dummy://"));
+  }
+};
+
+struct ASTDump : nixd::RecursiveASTVisitor<ASTDump> {
+
+  int Depth = 0;
+  std::unique_ptr<nix::EvalState> State;
+
+  bool traverseExpr(const nix::Expr *E) {
+    Depth++;
+    if (!nixd::RecursiveASTVisitor<ASTDump>::traverseExpr(E))
+      return false;
+    Depth--;
+    return true;
+  }
+
+#define NIX_EXPR(EXPR)                                                         \
+  bool visit##EXPR(const nix::EXPR *E) {                                       \
+    for (int i = 0; i < Depth; i++) {                                          \
+      std::cout << " ";                                                        \
+    }                                                                          \
+    std::cout << #EXPR << ": ";                                                \
+    E->show(State->symbols, std::cout);                                        \
+    std::cout << "\n";                                                         \
+    return true;                                                               \
+  }
+#include "nixd/NixASTNodes.inc"
+#undef NIX_EXPR
+};
+
+int main(int argc, char *argv[]) {
+  InitNix I;
+  auto State = I.getDummyState();
+  const auto *E = State->parseExprFromFile(argv[1]);
+  ASTDump A;
+  A.State = std::move(State);
+  A.traverseExpr(E);
+  return 0;
+}
