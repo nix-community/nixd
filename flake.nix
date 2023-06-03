0810fe6291f839c46cd4cf184f94ee54ff91e480
Ruixi-rebirth <ruixirebirth@gmail.com>
2023-06-03 11:23:09 +0800

    add overlay support
    add overlay support
    
    add overlay support

--- flake.nix
+++ flake.nix
@@ -1,20 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs = { nixpkgs, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = nixpkgs.lib.systems.flakeExposed;
+    imports = [
+      inputs.treefmt-nix.flakeModule
+      inputs.flake-root.flakeModule
+      inputs.mission-control.flakeModule
+      inputs.flake-parts.flakeModules.easyOverlay
+    ];
     perSystem = { config, self', inputs', pkgs, system, ... }:
       let
         nixd = pkgs.callPackage ./default.nix { };
       in
       {
-        packages = {
-          inherit nixd;
-          default = nixd;
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          programs.clang-format.enable = true;
+          package = pkgs.treefmt;
+          programs.nixpkgs-fmt.enable = true;
+          programs.prettier.enable = true;
         };
+        mission-control.scripts = {
+          fmt = {
+            description = "Format source code";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+        };
+
+        packages.default=nixd;
+        overlayAttrs = {
+          inherit (config.packages) nixd;
+        };
+        packages.nixd = nixd;
 
         devShells.default = nixd.overrideAttrs (old: {
           nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.clang-tools pkgs.gdb ];
@@ -25,6 +50,5 @@
           '';
         });
       };
-    systems = nixpkgs.lib.systems.flakeExposed;
   };
 }
