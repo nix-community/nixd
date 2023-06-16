cee0087b4229f0ed446ec06aafe24f227ed3e4e4
Yingchi Long <i@lyc.dev>
2023-06-16 22:18:22 +0800

    nixd/parser: fixup

--- lib/nixd/include/nixd/Parser.h
+++ lib/nixd/include/nixd/Parser.h
@@ -6,7 +6,17 @@ namespace nixd {
 
 using namespace nix;
 
-std::unique_ptr<ParseData> parse(char *text, size_t length, Pos::Origin origin,
-                                 const SourcePath &basePath, EvalState &state);
+
+std::unique_ptr<ParseData> parse(char *Text, size_t Length, Pos::Origin Origin,
+                                 const SourcePath &BasePath, EvalState &State,
+                                 std::shared_ptr<StaticEnv> Env);
+
+inline std::unique_ptr<ParseData> parse(char *Text, size_t Length,
+                                        Pos::Origin Origin,
+                                        const SourcePath &BasePath,
+                                        EvalState &State) {
+  return parse(Text, Length, std::move(Origin), BasePath, State,
+               State.staticBaseEnv);
+}
 
 } // namespace nixd
