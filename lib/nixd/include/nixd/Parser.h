55b06f539406135e7f3bcad555f98659995ab868
Yingchi Long <i@lyc.dev>
2023-06-17 23:57:45 +0800

    nixd/parser: add parser inline helpers function (for convenience)

--- lib/nixd/include/nixd/Parser.h
+++ lib/nixd/include/nixd/Parser.h
@@ -18,4 +18,20 @@ inline std::unique_ptr<ParseData> parse(char *Text, size_t Length,
                State.staticBaseEnv);
 }
 
+inline std::unique_ptr<ParseData> parse(std::string Text, Pos::Origin Origin,
+                                        const SourcePath &BasePath,
+                                        EvalState &State,
+                                        std::shared_ptr<StaticEnv> Env) {
+  Text.append("\0\0", 2);
+  return parse(Text.data(), Text.length(), std::move(Origin), BasePath, State,
+               std::move(Env));
+}
+
+inline std::unique_ptr<ParseData> parse(std::string Text, Pos::Origin Origin,
+                                        const SourcePath &BasePath,
+                                        EvalState &State) {
+  Text.append("\0\0", 2);
+  return parse(Text.data(), Text.length(), std::move(Origin), BasePath, State);
+}
+
 } // namespace nixd
