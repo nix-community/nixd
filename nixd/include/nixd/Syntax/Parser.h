f55cd8989b278c1ae7c7cfd6252468145745f4c8
Yingchi Long <i@lyc.dev>
2023-09-18 13:54:56 +0800

    nixd-lint: init

--- nixd/include/nixd/Syntax/Parser.h
+++ nixd/include/nixd/Syntax/Parser.h
@@ -4,4 +4,8 @@
 
 namespace nixd::syntax {
 void parse(char *Text, size_t Size, ParseData *Data);
+inline void parse(std::string Text, ParseData *Data) {
+  Text.append("\0\0", 2);
+  parse(Text.data(), Text.length(), Data);
+}
 } // namespace nixd::syntax
