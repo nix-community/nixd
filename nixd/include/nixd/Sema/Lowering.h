a3bc3898b66b9d6c3b91e1d04efcd67d44391bae
Yingchi Long <i@lyc.dev>
2023-09-20 09:58:29 +0800

    nixd/Sema: lowering for inherited attrs in binds

--- nixd/include/nixd/Sema/Lowering.h
+++ nixd/include/nixd/Sema/Lowering.h
@@ -9,6 +9,27 @@
 
 namespace nixd {
 
+struct Lowering;
+
+class ExprAttrsBuilder {
+  // The official parser does this work during the lowering process, via
+  // @addAttr in src/libexpr/parser.y
+
+  Lowering &LW;
+  nix::ExprAttrs *Result;
+
+  // Use a map to detect duplicated fields
+  // Not using the map inside nix::ExprAttrs because we want to preserve the
+  // range
+  std::map<nix::Symbol, syntax::Node *> Fields;
+
+public:
+  ExprAttrsBuilder(Lowering &LW);
+  void addAttribute(const syntax::Attribute &A);
+  void addInherited(const syntax::InheritedAttribute &IA);
+  nix::ExprAttrs *finish();
+};
+
 struct Lowering {
   nix::SymbolTable &STable;
   nix::PosTable &PTable;
@@ -18,6 +39,8 @@ struct Lowering {
   nix::Expr *lower(syntax::Node *Root);
   nix::ExprLambda *lowerFunction(syntax::Function *Fn);
   nix::Formal lowerFormal(const syntax::Formal &Formal);
+  nix::AttrPath lowerAttrPath(const syntax::AttrPath &Path);
+  nix::ExprAttrs *lowerBinds(const syntax::Binds &Binds);
 };
 
 } // namespace nixd
--- nixd/lib/Sema/Lowering.cpp
+++ nixd/lib/Sema/Lowering.cpp
@@ -3,7 +3,13 @@
 #include "nixd/Syntax/Diagnostic.h"
 #include "nixd/Syntax/Nodes.h"
 
+#include <nix/nixexpr.hh>
+
+#include <llvm/Support/FormatVariadic.h>
+
 namespace nixd {
+
+using syntax::Node;
 
 nix::Formal Lowering::lowerFormal(const syntax::Formal &Formal) {
   auto *Def = Lowering::lower(Formal.Default);
@@ -109,11 +115,112 @@ nix::ExprLambda *Lowering::lowerFunction(syntax::Function *Fn) {
   return NewLambda;
 }
 
+static syntax::Diagnostic mkAttrDupDiag(std::string Name, RangeIdx Range,
+                                        RangeIdx Prev) {
+  syntax::Diagnostic Diag;
+  Diag.Kind = syntax::Diagnostic::Error;
+  Diag.Msg = llvm::formatv("duplicated attr `{0}`", Name);
+  Diag.Range = Range;
+
+  syntax::Note Note;
+  Note.Msg = llvm::formatv("previously defined here");
+  Note.Range = Prev;
+  Diag.Notes.emplace_back(std::move(Note));
+  return Diag;
+}
+
+ExprAttrsBuilder::ExprAttrsBuilder(Lowering &LW) : LW(LW) {
+  Result = LW.Ctx.Pool.record(new nix::ExprAttrs);
+}
+
+nix::ExprAttrs *ExprAttrsBuilder::finish() { return Result; }
+
+void ExprAttrsBuilder::addAttribute(const syntax::Attribute &A) {}
+
+void ExprAttrsBuilder::addInherited(const syntax::InheritedAttribute &IA) {
+  if (IA.InheritedAttrs->Names.empty()) {
+    syntax::Diagnostic Diag;
+    Diag.Kind = syntax::Diagnostic::Warning;
+    Diag.Msg = "empty inherit expression";
+    Diag.Range = IA.Range;
+
+    LW.Diags.emplace_back(std::move(Diag));
+  }
+
+  // inherit (expr) attr1 attr2
+  // lowering `(expr)` to nix expression
+  nix::Expr *Subject = LW.lower(IA.E);
+
+  for (Node *Name : IA.InheritedAttrs->Names) {
+
+    // Extract nix::Symbol, report error if we encountered dynamic fields
+    nix::Symbol Sym;
+    switch (Name->getKind()) {
+    case Node::NK_Identifier: {
+      Sym = dynamic_cast<syntax::Identifier *>(Name)->Symbol;
+      break;
+    }
+    case Node::NK_String: {
+      Sym = LW.STable.create(dynamic_cast<syntax::String *>(Name)->S);
+      break;
+    }
+    default: {
+      syntax::Diagnostic Diag;
+      Diag.Kind = syntax::Diagnostic::Error;
+      Diag.Msg = "dynamic attributes are not allowed in inherit";
+      Diag.Range = Name->Range;
+
+      LW.Diags.emplace_back(std::move(Diag));
+    }
+    }
+
+    // Check if it is duplicated
+    if (Fields.contains(Sym)) {
+      std::string SymStr = LW.STable[Sym];
+      auto Diag = mkAttrDupDiag(SymStr, Name->Range, Fields[Sym]->Range);
+      LW.Diags.emplace_back(std::move(Diag));
+      continue;
+    }
+
+    // Insert an AttrDef to the result
+    Fields.insert({Sym, Name});
+    nix::Expr *AttrBody;
+    if (Subject) {
+      // inherit (expr) attr1 attr2
+      AttrBody = new nix::ExprSelect(Name->Range.Begin, Subject, Sym);
+    } else {
+      // inherit attr1 attr2 ...
+      AttrBody = new nix::ExprVar(Name->Range.Begin, Sym);
+    }
+    LW.Ctx.Pool.record(AttrBody);
+    nix::ExprAttrs::AttrDef Def(AttrBody, Name->Range.Begin, true);
+    Result->attrs.insert({Sym, Def});
+  }
+}
+
+nix::ExprAttrs *Lowering::lowerBinds(const syntax::Binds &Binds) {
+  ExprAttrsBuilder Builder(*this);
+
+  for (auto *Attr : Binds.Attributes) {
+    switch (Attr->getKind()) {
+    case Node::NK_Attribute: {
+      Builder.addAttribute(*dynamic_cast<syntax::Attribute *>(Attr));
+      break;
+    }
+    case Node::NK_InheritedAttribute: {
+      Builder.addInherited(*dynamic_cast<syntax::InheritedAttribute *>(Attr));
+      break;
+    }
+    default:
+      llvm_unreachable("must be A/IA! incorrect parsing rules?");
+    }
+  }
+  return Builder.finish();
+}
+
 nix::Expr *Lowering::lower(nixd::syntax::Node *Root) {
   if (!Root)
     return nullptr;
-
-  using syntax::Node;
 
   switch (Root->getKind()) {
   case Node::NK_Function: {
@@ -135,6 +242,17 @@ nix::Expr *Lowering::lower(nixd::syntax::Node *Root) {
     auto *NixWith =
         Ctx.Pool.record(new nix::ExprWith(With->Range.Begin, Attrs, Body));
     return NixWith;
+  }
+  case Node::NK_Binds: {
+    auto *Binds = dynamic_cast<syntax::Binds *>(Root);
+    return lowerBinds(*Binds);
+  }
+  case Node::NK_AttrSet: {
+    auto *AttrSet = dynamic_cast<syntax::AttrSet *>(Root);
+    assert(AttrSet->AttrBinds && "null AttrBinds of the AttrSet!");
+    nix::ExprAttrs *Binds = lowerBinds(*AttrSet->AttrBinds);
+    Binds->recursive = AttrSet->Recursive;
+    return Binds;
   }
   }
 
