29f093232b608f22227c18094662222637d1a221
Yingchi Long <i@lyc.dev>
2023-10-25 01:54:40 +0800

    libnixf/tools/nixf-ast-dump: use discarded trivia for testing

--- libnixf/tools/nixf-ast-dump/test/string.nix
+++ libnixf/tools/nixf-ast-dump/test/string.nix
@@ -1,2 +1,14 @@
-# RUN: nixf-ast-dump < %s | FileCheck %S/string.expected
+# RUN: nixf-ast-dump < %s | FileCheck %s
+
+# CHECK:      String
+# CHECK:      Token 9 asdasdasd
+# CHECK-NEXT: Interpolation 11
+# CHECK-NEXT:  Token 2 ${
+# CHECK-NEXT:  String 8
+# CHECK-NEXT:   Token 1  "
+# CHECK-NEXT:   Token 6 string
+# CHECK-NEXT:   Token 1 "
+# CHECK-NEXT:  Token 1  }
+# CHECK-NEXT: Token 6 asdasd
+# CHECK-NEXT: Token 1 "
 "asdasdasd${ "string" }asdasd"
