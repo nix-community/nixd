1800ce862de5284f572c0fee5c8d72fde3b44a84
Yingchi Long <i@lyc.dev>
2023-10-29 03:16:28 +0800

    libnixf/tools/nixf-ast-dump: add some attrset tests

--- libnixf/tools/nixf-ast-dump/test/attrset-simple.nix
+++ libnixf/tools/nixf-ast-dump/test/attrset-simple.nix
@@ -16,5 +16,49 @@
   # CHECK:   Token 1 c
   a.b.c = 54;
 
-  a
+
+  #      CHECK:     AttrPath 5
+  # CHECK-NEXT:       Token 1 a
+  # CHECK-NEXT:       Token 1 .
+  # CHECK-NEXT:       Token 1 b
+  # CHECK-NEXT:       Token 1 .
+  # CHECK-NEXT:       Token 1 c
+  # CHECK-NEXT:      Token 1 =
+  # CHECK-NEXT:      Token 1 1
+  # CHECK-NEXT:      Token 1 ;
+    p = {a.b.c=1;};
+
+
+  # CHECK: AttrSet 27
+  # CHECK-NEXT:    Token 2 {
+  # CHECK-NEXT:    Binds 21
+  # CHECK-NEXT:     Binding 21
+  # CHECK-NEXT:      AttrPath 16
+  # CHECK-NEXT:       String 11
+  # CHECK-NEXT:        Token 6 "
+  # CHECK-NEXT:        Interpolation 4
+  # CHECK-NEXT:         Token 2 ${
+  # CHECK-NEXT:         Token 1 c
+  # CHECK-NEXT:         Token 1 }
+  # CHECK-NEXT:        Token 1 "
+  # CHECK-NEXT:       Token 1 .
+  # CHECK-NEXT:       Interpolation 4
+  # CHECK-NEXT:        Token 2 ${
+  # CHECK-NEXT:        Token 1 d
+  # CHECK-NEXT:        Token 1 }
+  # CHECK-NEXT:      Token 2 =
+  # CHECK-NEXT:      Token 2 3
+  # CHECK-NEXT:      Token 1 ;
+  # CHECK-NEXT:    Token 4 }
+  # CHECK-NEXT:   Token 1 ;
+  dynamic = {
+    "${c}".${d} = 3;
+  };
+
+
+  # CHECK: AttrSet 3
+  # CHECK-NEXT:   Token 2 {
+  # CHECK-NEXT:   Binds 0
+  # CHECK-NEXT:   Token 1 }
+  empty = {};
     }
