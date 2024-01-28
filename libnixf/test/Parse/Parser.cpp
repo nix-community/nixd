607a6a76bea429f1e206963c4598662e2ef04603
Yingchi Long <i@lyc.dev>
2024-01-28 14:47:55 +0800

    libnixf/Parse: add unit test for parsing expr_select

--- libnixf/test/Parse/Parser.cpp
+++ libnixf/test/Parse/Parser.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "Parser.h"
+
 #include "nixf/Basic/Diagnostic.h"
 #include "nixf/Basic/Nodes.h"
 #include "nixf/Basic/Range.h"
@@ -847,6 +849,116 @@ rec {
   ASSERT_TRUE(F.oldRange().lCur().isAt(2, 2, 9));
   ASSERT_TRUE(F.oldRange().rCur().isAt(3, 13, 26));
   ASSERT_EQ(F.newText(), "");
+}
+
+TEST(Parser, SelectOK) {
+  auto Src = R"(a.b.c)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExprSelect();
+
+  ASSERT_TRUE(AST);
+
+  ASSERT_EQ(Diags.size(), 0);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprSelect);
+  ASSERT_TRUE(AST->range().lCur().isAt(0, 0, 0));
+  ASSERT_TRUE(AST->range().rCur().isAt(0, 5, 5));
+
+  const auto &S = static_cast<ExprSelect *>(AST.get());
+
+  ASSERT_EQ(S->defaultExpr(), nullptr);
+  ASSERT_TRUE(S->path()->lCur().isAt(0, 2, 2));
+  ASSERT_TRUE(S->path()->rCur().isAt(0, 5, 5));
+}
+
+TEST(Parser, SelectSimple) {
+  auto Src = R"(1)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExprSelect();
+
+  ASSERT_TRUE(AST);
+
+  ASSERT_EQ(Diags.size(), 0);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprInt);
+}
+
+TEST(Parser, SelectDefault) {
+  auto Src = R"(a.c or b)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExprSelect();
+
+  ASSERT_TRUE(AST);
+
+  ASSERT_EQ(Diags.size(), 0);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprSelect);
+
+  const auto &S = static_cast<ExprSelect *>(AST.get());
+
+  ASSERT_TRUE(S->defaultExpr()->range().lCur().isAt(0, 7, 7));
+  ASSERT_TRUE(S->defaultExpr()->range().rCur().isAt(0, 8, 8));
+  ASSERT_EQ(S->defaultExpr()->kind(), Node::NK_ExprVar);
+}
+
+TEST(Parser, SelectExtraDot) {
+  auto Src = R"(a.)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExprSelect();
+
+  ASSERT_TRUE(AST);
+
+  ASSERT_EQ(Diags.size(), 1);
+  ASSERT_EQ(Diags[0].kind(), Diagnostic::DK_SelectExtraDot);
+  ASSERT_TRUE(Diags[0].range().lCur().isAt(0, 1, 1));
+  ASSERT_TRUE(Diags[0].range().rCur().isAt(0, 2, 2));
+
+  // Check the note.
+  ASSERT_EQ(Diags[0].notes().size(), 0);
+
+  // Check fix-it hints.
+  ASSERT_EQ(Diags[0].fixes().size(), 2);
+  ASSERT_EQ(Diags[0].fixes()[0].edits().size(), 1);
+  ASSERT_EQ(Diags[0].fixes()[0].message(), "remove extra .");
+  const auto &F = Diags[0].fixes()[0].edits()[0];
+  ASSERT_TRUE(F.oldRange().lCur().isAt(0, 1, 1));
+  ASSERT_TRUE(F.oldRange().rCur().isAt(0, 2, 2));
+
+  ASSERT_EQ(Diags[0].fixes()[1].edits().size(), 1);
+  ASSERT_EQ(Diags[0].fixes()[1].message(), "insert dummy attrpath");
+  const auto &F2 = Diags[0].fixes()[1].edits()[0];
+  ASSERT_TRUE(F2.oldRange().lCur().isAt(0, 2, 2));
+  ASSERT_TRUE(F2.oldRange().rCur().isAt(0, 2, 2));
+  ASSERT_EQ(F2.newText(), "\"dummy\"");
+}
+
+TEST(Parser, SelectExtraOr) {
+  auto Src = R"(a.c or)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExprSelect();
+
+  ASSERT_TRUE(AST);
+
+  ASSERT_EQ(Diags.size(), 1);
+  ASSERT_EQ(Diags[0].kind(), Diagnostic::DK_Expected);
+
+  // Check the note.
+  ASSERT_EQ(Diags[0].notes().size(), 0);
+
+  // Check fix-it hints.
+  ASSERT_EQ(Diags[0].fixes().size(), 2);
+  ASSERT_EQ(Diags[0].fixes()[1].edits().size(), 1);
+  ASSERT_EQ(Diags[0].fixes()[1].message(), "remove `or` keyword");
+  const auto &F = Diags[0].fixes()[1].edits()[0];
+  ASSERT_TRUE(F.oldRange().lCur().isAt(0, 4, 4));
+  ASSERT_TRUE(F.oldRange().rCur().isAt(0, 6, 6));
 }
 
 } // namespace
