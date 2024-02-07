2f5f22c282467de41bdd31d8474dd06dbe2823af
Yingchi Long <i@lyc.dev>
2024-02-07 19:17:47 +0800

    libnixf/Parse: unit tests for expr dispatching

--- libnixf/test/Parse/Parser.cpp
+++ libnixf/test/Parse/Parser.cpp
@@ -1233,4 +1233,98 @@ TEST(Parser, ParseExprLambda) {
   ASSERT_EQ(Diags.size(), 0);
 }
 
+TEST(Parser, ExprDispatch_id_at) {
+  // ID @
+  auto Src = R"(a @)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
+TEST(Parser, ExprDispatch_id_colon) {
+  // ID :
+  auto Src = R"(a :)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
+TEST(Parser, ExprDispatch_r_curly_id_colon) {
+  // { ID :
+  auto Src = R"({ a :)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
+TEST(Parser, ExprDispatch_r_curly_id_at) {
+  // { ID @
+  auto Src = R"({ a @)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
+TEST(Parser, ExprDispatch_r_curly_id_question) {
+  // { ID ?
+  auto Src = R"({ a ?)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+TEST(Parser, ExprDispatch_r_curly_id_comma) {
+  // { ID ,
+  auto Src = R"({ a ,)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+TEST(Parser, ExprDispatch_r_curly_id_id) {
+  // { ID ID
+  auto Src = R"({ a b)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
+TEST(Parser, ExprDispatch_r_curly_id_ellipsis) {
+  // { ID ...
+  auto Src = R"({ a ...)"sv;
+
+  std::vector<Diagnostic> Diags;
+  Parser P(Src, Diags);
+  auto AST = P.parseExpr();
+
+  ASSERT_TRUE(AST);
+  ASSERT_EQ(AST->kind(), Node::NK_ExprLambda);
+}
+
 } // namespace
