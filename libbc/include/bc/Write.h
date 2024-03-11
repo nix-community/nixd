a5758003c47f3831abbb85b80bcafaca3eeb9b33
Yingchi Long <i@lyc.dev>
2024-03-12 00:45:21 +0800

    libbc: avoid using template specialization

--- libbc/include/bc/Write.h
+++ libbc/include/bc/Write.h
@@ -1,14 +1,10 @@
 #pragma once
 
 #include <ostream>
+#include <sstream>
 #include <type_traits>
 
 namespace bc {
-
-template <class T> void writeBytecode(std::ostream &OS, const T &Data) {
-  static_assert(!std::is_same_v<T, T> &&
-                "No writeBytecode implementation for this type");
-}
 
 /// \brief Basic primitives. Trivial data types are just written to a stream.
 template <class T>
@@ -17,14 +13,16 @@ void writeBytecode(std::ostream &OS, const T &Data) {
   OS.write(reinterpret_cast<const char *>(&Data), sizeof(T));
 }
 
-template <>
-void writeBytecode<std::string_view>(std::ostream &OS,
-                                     const std::string_view &Data);
+void writeBytecode(std::ostream &OS, const std::string_view &Data);
 
-template <>
-inline void writeBytecode<std::string>(std::ostream &OS,
-                                       const std::string &Data) {
+inline void writeBytecode(std::ostream &OS, const std::string &Data) {
   writeBytecode(OS, std::string_view(Data));
+}
+
+template <class T> std::string toBytecode(const T &Data) {
+  std::ostringstream OS;
+  writeBytecode(OS, Data);
+  return OS.str();
 }
 
 } // namespace bc
--- libbc/src/Write.cpp
+++ libbc/src/Write.cpp
@@ -2,9 +2,7 @@
 
 namespace bc {
 
-template <>
-void writeBytecode<std::string_view>(std::ostream &OS,
-                                     const std::string_view &Data) {
+void writeBytecode(std::ostream &OS, const std::string_view &Data) {
   writeBytecode(OS, static_cast<std::size_t>(Data.size()));
   OS.write(Data.data(), static_cast<std::streamsize>(Data.size()));
 }
