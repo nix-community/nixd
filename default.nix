4bf7f6ee1fb5ba2de652e889c306e796565fd5e9
Yingchi Long <longyingchi24s@ict.ac.cn>
2025-05-10 02:52:01 +0800

    vendor: bump version 2.6.2 → 2.6.4

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@
 
 stdenv.mkDerivation {
   pname = "nixd";
-  version = "2.6.3";
+  version = "2.6.4";
 
   src = ./.;
 
--- libnixf/default.nix
+++ libnixf/default.nix
@@ -13,7 +13,7 @@
 
 stdenv.mkDerivation {
   pname = "nixf";
-  version = "2.6.3";
+  version = "2.6.4";
 
   src = ../.;
 
