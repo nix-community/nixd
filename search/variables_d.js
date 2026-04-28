a6c19e3f24ab6466e200906735b1c155410a074f
inclyc <inclyc@users.noreply.github.com>
2026-04-28 03:09:24 +0000

    deploy: 6bf30951a3dc407a798d30db427e3f96ac9b39f5

---
+++ search/variables_d.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['offsetencoding_0',['offsetEncoding',['../structlspserver_1_1ClientCapabilities.html#ae2666d024c096114db82d579cd6052b2',1,'lspserver::ClientCapabilities']]],
+  ['offsetsinsignaturehelp_1',['OffsetsInSignatureHelp',['../structlspserver_1_1ClientCapabilities.html#ad609bc3de9a9964c4663cd2d16d5a58a',1,'lspserver::ClientCapabilities']]],
+  ['olduri_2',['oldUri',['../structlspserver_1_1RenameFile.html#addecbc4c20b52cbe97cba6050bbf0cfb',1,'lspserver::RenameFile']]],
+  ['only_3',['only',['../structlspserver_1_1CodeActionContext.html#a7ff6fc854befe3a80eca853cc990b84d',1,'lspserver::CodeActionContext']]],
+  ['optioncomplete_4',['OptionComplete',['../namespacenixd_1_1rpcMethod.html#adc49a105932b9d0b77397ccce92c8367',1,'nixd::rpcMethod']]],
+  ['optioninfo_5',['OptionInfo',['../namespacenixd_1_1rpcMethod.html#aafd8d0256da5ab1d47b355045f5b189a',1,'nixd::rpcMethod']]],
+  ['options_6',['options',['../structnixd_1_1Configuration.html#a7693c8cb09c708f4db3f0577867cce0c',1,'nixd::Configuration::options'],['../structlspserver_1_1CreateFile.html#a1e60587d9f97a3b3646be1096714674a',1,'lspserver::CreateFile::options'],['../structlspserver_1_1RenameFile.html#a0e1008a1da6bcf782e24d67ca78ee53b',1,'lspserver::RenameFile::options'],['../structlspserver_1_1DeleteFile.html#ad6ddfae08448b550eb82bae65606b026',1,'lspserver::DeleteFile::options']]],
+  ['output_7',['output',['../namespaceDiagnostic.html#a2e5522440453c79ff0deb0966ff35eff',1,'Diagnostic.output'],['../namespaceDiagnosticEnum.html#a6f090de724ec83ad8e13c1b879fe0ed1',1,'DiagnosticEnum.output']]],
+  ['overwrite_8',['overwrite',['../structlspserver_1_1CreateFileOptions.html#a18aff0acb87145ee79b34c85e33d4607',1,'lspserver::CreateFileOptions::overwrite'],['../structlspserver_1_1RenameFileOptions.html#a1b6a702751f07ef86ee46038fabcfa46',1,'lspserver::RenameFileOptions::overwrite']]]
+];
